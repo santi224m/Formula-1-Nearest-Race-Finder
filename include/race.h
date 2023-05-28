5a8cf8be588ce712a4ff8fec4b27f2c02662cda7
Santiago Mondragon <santiago.mondragon224@gmail.com>
2023-05-28 12:10:31 -0700

    Implement race.h

--- include/race.h
+++ include/race.h
@@ -3,19 +3,32 @@
 
 #include <ctime>
 #include <string>
+#include <iostream>
 
-struct Coordinate;
+struct Coordinate {
+  double latitude;
+  double longitude;
+
+  // CTOR
+  Coordinate(double lat, double lon) : latitude(lat), longitude(lon) {}
+};
 
 struct Race {
   std::string name;
   std::string circuit;
   Coordinate coordinate;
   std::tm date;
-};
 
-struct Coordinate {
-  double latitude;
-  double longitude;
+  // CTOR
+  Race(const std::string& name, const std::string& circuit, 
+       const Coordinate& coordinate, const std::tm& date)
+    : name(name), circuit(circuit), coordinate(coordinate), date(date) {}
+
+  void PrintDate() {
+    std::cout << date.tm_mon + 1 << "-"
+              << date.tm_mday << "-"
+              << date.tm_year + 1900;
+  }
 };
 
 #endif  // RACE_H
