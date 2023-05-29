db92e36a358fc851b4fad3feae96c9dfc808ba05
Santiago Mondragon <santiago.mondragon224@gmail.com>
2023-05-29 12:40:13 -0700

    Test quadtree insertion method

--- test/sandbox.cpp
+++ test/sandbox.cpp
@@ -1,14 +1,37 @@
 #include "../include/quadtree.h"
 
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
+
 int main() {
-  Coordinate c(30.1333, -97.6411);
-  std::tm date = {};
-  date.tm_year = 2023;
-  date.tm_mon = 10;
-  date.tm_mday = 22;
-  Race USGP("United States Grand Prix", "Circuit of The Americas", c, date);
-  QuadTree myTree;
-  myTree.insert(USGP);
+  std::ifstream csv_file("../data/formula1_grand_prix_2023.csv");
+  std::string line;
+  std::vector<Race> races;
+  QuadTree qt;
+
+  while(std::getline(csv_file,line)) {
+    std::istringstream ss(line);
+    std::string race_name, circuit_name, lat_str, lon_str, date;
+    std::getline(ss, race_name, ',');
+    std::getline(ss, circuit_name, ',');
+    std::getline(ss, lat_str, ',');
+    std::getline(ss, lon_str, ',');
+    std::getline(ss, date, ',');
+    double latitude = std::stod(lat_str);
+    double longitude = std::stod(lon_str);
+
+    // Parse time  into tm stuct
+    std::tm parsedTime = {};
+    std::istringstream tmss(date);
+    tmss >> std::get_time(&parsedTime, "%Y-%m-%d");
+    
+    Race r(race_name, circuit_name, {latitude, longitude}, parsedTime);
+    qt.insert(r);
+  }
 
   return 0;
 }
